112c8292ce82e42103feba1492167ede50ce2f4a
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2024-07-31 05:23:01 +0000

    [LeetCode Sync] Runtime - 6 ms (0.40%), Memory - 13.4 MB (99.98%)

--- 0898-transpose-matrix/solution.cpp
+++ 0898-transpose-matrix/solution.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-         int m = matrix.size();
-    int n = matrix[0].size();
-
-    // Initialize the transpose matrix with dimensions swapped
-    vector<vector<int>> result(n, vector<int>(m, 0));
-
-    // Populate the transpose matrix
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            result[j][i] = matrix[i][j];
-        }
-    }
-
-    return result;
+    vector<vector<int>> transpose(vector<vector<int>>& p) {
         
+        int m= p.size();
+        int n= p[0].size();
+        vector<vector<int>> t(n,vector<int>(m));
+        for(int i=0; i<n;i++){
+            for(int j=0; j<m; j++){
+                t[i][j]=p[j][i];
+            }
+        }
+       return t;
     }
 };
