3cbee6c6701cbd1c7cdc5ddd08f55fc5b70416cc
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2024-08-22 20:57:53 +0000

    [LeetCode Sync] Runtime - 30 ms (99.69%), Memory - 17.4 MB (99.69%)

---
+++ 0160-intersection-of-two-linked-lists/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* ta = headA;
+        ListNode* tb = headB;
+        int ca = 0;
+        while (ta != nullptr) {
+            ta = ta->next;
+            ca++;
+        }
+        int cb = 0;
+        while (tb != nullptr) {
+            tb = tb->next;
+            cb++;
+        }
+        ta = headA;
+        tb = headB;
+        if (ca > cb) {
+            int diff = ca - cb;
+            for (int i = 0; i < diff; i++) {
+                ta = ta->next;
+            }
+        } else {
+            int diff2 = cb - ca;
+            for (int i = 0; i < diff2; i++) {
+                tb = tb->next;
+            }
+        }
+        while ( tb != nullptr) {
+            if (ta == tb) {
+                return ta;
+            }
+            ta = ta->next;
+            tb = tb->next;
+        }
+        return NULL;
+    }
+};
