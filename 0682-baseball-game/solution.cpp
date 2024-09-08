8cce532e5be58237c45561247c9efd72496e85bc
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2024-09-08 10:27:41 +0000

    [LeetCode Sync] Runtime - 7 ms (1.73%), Memory - 12 MB (81.42%)

---
+++ 0682-baseball-game/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int calPoints(vector<string>& op) {
+        stack<int> s;
+        for (int i = 0; i < op.size(); i++) {
+            if (op[i].size() > 1 ||
+                (op[i].size() == 1 && op[i][0] >= '0' && op[i][0] <= '9'))
+                s.push(stoi(op[i]));
+            else if (op[i] == "C")
+                s.pop();
+            else if (op[i] == "D")
+                s.push(2 * s.top());
+            else {
+                int val1 = s.top();
+                s.pop();
+                int sum = val1 + s.top();
+                s.push(val1);
+                s.push(sum);
+            }
+        }
+        int sum = 0;
+        while (!s.empty()) {
+            sum += s.top();
+            s.pop();
+        }
+        return sum;
+    }
+};
