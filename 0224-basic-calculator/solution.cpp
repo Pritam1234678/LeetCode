6cfdc2bab6999c6221f5db757eab6c1e6dbc481d
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2024-09-09 15:53:13 +0000

    [LeetCode Sync] Runtime - 9 ms (14.83%), Memory - 10 MB (100.00%)

---
+++ 0224-basic-calculator/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> nums; // Stack to store numbers
+        stack<char> ops; // Stack to store operators
+        int num = 0;
+        int sign = 1; // 1 means positive, -1 means negative
+        int result = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '+' || c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = (c == '+') ? 1 : -1;
+            } else if (c == '(') {
+                nums.push(result);
+                ops.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num;
+                num = 0;
+                result *= ops.top();
+                ops.pop();
+                result += nums.top();
+                nums.pop();
+            }
+        }
+        result += sign * num;
+        return result;
+    }
+};
+
