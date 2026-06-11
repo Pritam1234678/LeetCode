82cd507d73bdfedb15e3f004fdb34dd019312e39
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2026-06-11 03:53:02 +0000

    [LeetCode Sync] Runtime - 1 ms (59.65%), Memory - 47.6 MB (68.24%)

--- 0042-trapping-rain-water/solution.java
+++ 0042-trapping-rain-water/solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
-
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = Math.max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = Math.max(rightMax, height[right]);
-                water += rightMax - height[right];
-            }
+        int n =height.length;
+        int pre[]= new int[n];
+        pre[0]=-1;
+        int max= height[0];
+        for(int i=1; i<n;i++){
+            pre[i]=max;
+            max=Math.max(height[i],max);
         }
-
-        return water;        
+         max =height[n-1];
+        pre[n-1]=-1;
+        for(int i=n-2;i>=0;i--){
+            if(max<pre[i])
+            pre[i]=max;
+            max=Math.max(height[i],max);
+        }
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            if(height[i]<pre[i])ans+=pre[i]-height[i];
+        }
+        return ans;
     }
 }
