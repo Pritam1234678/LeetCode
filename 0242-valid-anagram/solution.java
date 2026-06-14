91c2351b4708744479e7cb6671e742d9905ee4db
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2026-06-14 08:50:07 +0000

    [LeetCode Sync] Runtime - 5 ms (61.90%), Memory - 44.2 MB (89.32%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,15 +1,21 @@
 class Solution {
-       public static String sortString(String str) {
-        char[] charArray = str.toCharArray();
-        Arrays.sort(charArray);
-        return new String(charArray);
-    }
     public boolean isAnagram(String s, String t) {
-         if (s.length()!=t.length()) return false;
-           String a=sortString(s);
-           String b=sortString(t);
-           if (a.equals(b)) return true;
-           return false;
-           
+        if (s.length() != t.length())
+            return false;
+        int freq[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+
+            int a = s.charAt(i) - 'a';
+            int b = t.charAt(i) - 'a';
+            freq[a] += 1;
+            freq[b] -= 1;
+
+        }
+        for (int x : freq) {
+            if (x != 0)
+                return false;
+        }
+        return true;
+
     }
 }
