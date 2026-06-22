06358d8fc8c235e13024749d794644e719e7d219
Pritam Mandal <89439754+Pritam1234678@users.noreply.github.com>
2026-06-22 08:33:05 +0000

    [LeetCode Sync] Runtime - 1 ms (99.96%), Memory - 46.2 MB (79.16%)

---
+++ 0383-ransom-note/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] mag = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            mag[ch - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            mag[ch - 'a']--;
+            if (mag[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
